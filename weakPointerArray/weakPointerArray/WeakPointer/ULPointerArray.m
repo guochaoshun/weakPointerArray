871157e92554ca4bd745220ad5a13b937da48500
郭朝顺 <657257897@qq.com>
2021-11-26 10:37:59 +0800

    [comments]   补充注释; 优化删除nil的方法 [jira_id] none [version]

--- weakPointerArray/weakPointerArray/WeakPointer/ULPointerArray.m
+++ weakPointerArray/weakPointerArray/WeakPointer/ULPointerArray.m
@@ -67,7 +67,23 @@
 }
 
 - (void)removeNilPointerIfNeed {
+    // pointerArray有个bug,
+//    如果不调用[pointerArray addPointer:nil],直接调用-compact,
+//    不会移除pointerArray弱引用自动置nil的指针,导致pointerArray的下表和pointerArray下标不一致
+//    pointerArray数组存在nil时，而allobject的值不包含nil
+//    参考链接：https://stackoverflow.com/questions/31322290/nspointerarray-weird-compaction
+
+    // 使用下面的写法, 可以保证把pointerArray中的所有nil移除掉
+    // 方案1: 直接移除,O(n)复杂度
+    [self.weakPointerArray addPointer:nil];
     [self.weakPointerArray compact];
+    // 方案2: 比较导出数据的count和自身的count,不一致在做移除,
+    // 导出这个数组需要O(n),移除操作需要O(n),总体是O(2*n)复杂度,还不如方案1的做法,不推荐
+//    NSArray *result = self.weakPointerArray.allObjects;
+//    if (self.count != result.count) {
+//        [self.weakPointerArray addPointer:nil];
+//        [self.weakPointerArray compact];
+//    }
 }
 
 
